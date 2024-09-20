a2bb775aa7ed84337a992ccfac982cb83dfecb57
Ashba Aslam <ashbaaslam6@gmail.com>
2024-09-20 21:19:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+//   QNO:1   //
+var name = "Ashba Aslam";
+alert(name);
+
+//    QNO:2   //
+
+var x="Ashba Aslam"
+alert("Hello ,my name is"+" "+x);
+console.log(x);
+// ----------------//
+
+
+//    QNO:3    //
+ 
+var a =4;
+var b =2;
+ console.log
+ (a++ + --b - --a + b++ + a+b);
+
+//  QNO:  4  //
+var uniform = prompt("uniform clean or not")
+var shoes = prompt(" shoes clean or not")
+var nails = prompt("are your nails done")
+var hair = prompt("Did you comb your hair")
+var age = prompt("How old are you")
+if (uniform ===  "clean" && shoes=== "clean" && nails === "yes" && hair === "yes"  && age <= 5) {
+ alert("go to primary");
+} else if ("age  >= 11"){
+    alert("go to secondary");
+}
